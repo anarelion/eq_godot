98a4f73e844cabefc193a5dd3b762c0e5ddb3ead
Anarelion <anarelion@gmail.com>
2024-05-15 12:20:37 +0100

    Add suppression for notice of required minimum windows version

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Interoperability",
+    "CA1416:Validate platform compatibility", 
+    Justification = "",
+    Scope = "member",
+    Target = "~M:EQGodot2.resource_manager.pack_file.PFSArchive.ProcessBMPImage(EQGodot2.resource_manager.pack_file.PFSFile)~Godot.ImageTexture"
+)]
